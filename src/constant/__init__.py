3797fd92bbf4f550cb760dadbadffa31bd2cf94b
milanbeherazyx <milanbeherazyx@gmail.com>
2023-07-16 09:09:05 +0530

    AWS

---
+++ src/constant/__init__.py
@@ -0,0 +1,12 @@
+import os
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
+
+TARGET_COLUMN = "DEFAULT"
+MONGO_DB_URL="mongodb+srv://milanbeherazyx:VKF4Yq7WqLwGIpkG@cluster0.wui6u7y.mongodb.net/?retryWrites=true&w=majority"
+
+DATABASE_NAME="Project"
+COLLECTION_NAME="Credit_Card_Fault"
