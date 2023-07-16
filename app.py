2681d2a996e4faa8d0b06406410db4d9733b1c0b
Milan Kumar Behera <112469564+milanbeherazyx@users.noreply.github.com>
2023-07-16 09:33:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def predict_datapoint():
     return render_template('home.html', results=result_value)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=8080)
